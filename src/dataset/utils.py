c4256fab6ef5fa17f26db2c9a3c8ff0f6f25bef3
Lee Yamin <yaminleej@gmail.com>
2023-10-03 12:57:13 +0300

    darts package working for 1 time-series df

--- src/dataset/utils.py
+++ src/dataset/utils.py
@@ -103,74 +103,92 @@ def prepare_data_for_run(data):
 
     print(f"Subsampling data from 1 sample per 1 minute to 1 sample per {config.sub_sample_rate} minutes.")
     data.sub_sample_data(sub_sample_rate=config.sub_sample_rate, aggregation_type=config.aggregation_type)
-    print("Splitting data into train and test.")
-    train_dataset, test_dataset = data.split_to_train_and_test_Lee()
-    print(f"Amount of dataframes in train data is {len(train_dataset)}, "
-          f"with {sum(len(df) for df in train_dataset)} time samples.")
-    print(f"Amount of dataframes in test data is {len(test_dataset)}, "
-          f"with {sum(len(df) for df in test_dataset)} time samples.")
+    # print("Splitting data into train and test.")
+    # train_dataset, test_dataset = data.split_to_train_and_test_Lee()
+    # print(f"Amount of dataframes in train data is {len(train_dataset)}, "
+    #       f"with {sum(len(df) for df in train_dataset)} time samples.")
+    # print(f"Amount of dataframes in test data is {len(test_dataset)}, "
+    #       f"with {sum(len(df) for df in test_dataset)} time samples.")
+    #
+    # print("Preparing data for time series.")
+    # train_dataset.prepare_dataset_for_time_series(look_back=config.look_back, horizon=config.horizon)
+    # test_dataset.prepare_dataset_for_time_series(look_back=config.look_back, horizon=config.horizon)
+    #
+    # train_dataset.record_df_indices()
+    # test_dataset.record_df_indices()
+    #
+    # train_dataset.concatenate_dfs()
+    # test_dataset.concatenate_dfs()
 
-    print("Preparing data for time series.")
-    train_dataset.prepare_dataset_for_time_series(look_back=config.look_back, horizon=config.horizon)
-    test_dataset.prepare_dataset_for_time_series(look_back=config.look_back, horizon=config.horizon)
+    data.record_df_indices()
 
-    train_dataset.record_df_indices()
-    test_dataset.record_df_indices()
+    data.concatenate_dfs()
 
-    train_dataset.concatenate_dfs()
-    test_dataset.concatenate_dfs()
+    # TODO: instead of deepcopy, save in the class
+    data_set = deepcopy(data.dfs_concatenated)
+    data_set = data_set[['sample', 'time', 'source_df_idx']]
 
-    train_set = deepcopy(train_dataset.dfs_concatenated)
-    test_set = deepcopy(test_dataset.dfs_concatenated)
-    train_set_source_df_idx = deepcopy(train_set.source_df_idx).to_numpy()
-    test_set_source_df_idx = deepcopy(test_set.source_df_idx).to_numpy()
-    train_set.drop('source_df_idx', axis=1, inplace=True)
-    test_set.drop('source_df_idx', axis=1, inplace=True)
+    # train_set = deepcopy(train_dataset.dfs_concatenated)
+    # test_set = deepcopy(test_dataset.dfs_concatenated)
+    # train_set_source_df_idx = deepcopy(train_set.source_df_idx).to_numpy()
+    # test_set_source_df_idx = deepcopy(test_set.source_df_idx).to_numpy()
+    # train_set.drop('source_df_idx', axis=1, inplace=True)
+    # test_set.drop('source_df_idx', axis=1, inplace=True)
 
-    train_set_as_np = train_set.to_numpy()
-    test_set_as_np = test_set.to_numpy()
+    # train_set_as_np = train_set.to_numpy()
+    # test_set_as_np = test_set.to_numpy()
 
-    print("Preparing data for training.")
-    x_train, y_train = split_data_into_x_and_y(train_set_as_np, config.look_back, config.horizon)
-    x_test, y_test = split_data_into_x_and_y(test_set_as_np, config.look_back, config.horizon)
+    # print("Preparing data for training.")
+    # x_train, y_train = split_data_into_x_and_y(train_set_as_np, config.look_back, config.horizon)
+    # x_test, y_test = split_data_into_x_and_y(test_set_as_np, config.look_back, config.horizon)
+    # transform data
+    epsilon = 1e-8
+    data_set['sample'] = np.log(data_set['sample'] + epsilon)
 
-    print(f"Transforming and scaling data using {config.transformation_method} transformation "
-          f"and {config.scale_method} scaling.")
-    x_train, scaling_attributes = transform_and_scale_data(x_train,
-                                                           transformation_method=config.transformation_method,
-                                                           scale_method=config.scale_method,
-                                                           scaling_attributes=None)
-    x_test, _ = transform_and_scale_data(x_test,
-                                         transformation_method=config.transformation_method,
-                                         scale_method=config.scale_method,
-                                         scaling_attributes=scaling_attributes)
-    y_train, _ = transform_and_scale_data(y_train,
-                                          transformation_method=config.transformation_method,
-                                          scale_method=config.scale_method,
-                                          scaling_attributes=scaling_attributes)
-    y_test, _ = transform_and_scale_data(y_test,
-                                         transformation_method=config.transformation_method,
-                                         scale_method=config.scale_method,
-                                         scaling_attributes=scaling_attributes)
+    # scale the data
+    min = data_set['sample'].min()
+    max = data_set['sample'].max()
+    data_set['sample'] = (data_set['sample'] - min) / (max - min)
 
-    # flip the data to be along the time axis
-    x_train = deepcopy(np.flip(x_train, axis=1))
-    x_test = deepcopy(np.flip(x_test, axis=1))
+    # print(f"Transforming and scaling data using {config.transformation_method} transformation "
+    #       f"and {config.scale_method} scaling.")
+    # x_train, scaling_attributes = transform_and_scale_data(x_train,
+    #                                                        transformation_method=config.transformation_method,
+    #                                                        scale_method=config.scale_method,
+    #                                                        scaling_attributes=None)
+    # x_test, _ = transform_and_scale_data(x_test,
+    #                                      transformation_method=config.transformation_method,
+    #                                      scale_method=config.scale_method,
+    #                                      scaling_attributes=scaling_attributes)
+    # y_train, _ = transform_and_scale_data(y_train,
+    #                                       transformation_method=config.transformation_method,
+    #                                       scale_method=config.scale_method,
+    #                                       scaling_attributes=scaling_attributes)
+    # y_test, _ = transform_and_scale_data(y_test,
+    #                                      transformation_method=config.transformation_method,
+    #                                      scale_method=config.scale_method,
+    #                                      scaling_attributes=scaling_attributes)
+
+    # # flip the data to be along the time axis
+    # x_train = deepcopy(np.flip(x_train, axis=1))
+    # x_test = deepcopy(np.flip(x_test, axis=1))
 
     # reshape the data to be in the format for torch
-    x_train = x_train.reshape((-1, config.look_back, 1))
-    x_test = x_test.reshape((-1, config.look_back, 1))
-    y_train = y_train.reshape((-1, config.horizon, 1))
-    y_test = y_test.reshape((-1, config.horizon, 1))
-    train_set_source_df_idx = train_set_source_df_idx.reshape((-1, 1))
-    test_set_source_df_idx = test_set_source_df_idx.reshape((-1, 1))
+    # x_train = x_train.reshape((-1, config.look_back, 1))
+    # x_test = x_test.reshape((-1, config.look_back, 1))
+    # y_train = y_train.reshape((-1, config.horizon, 1))
+    # y_test = y_test.reshape((-1, config.horizon, 1))
+    # train_set_source_df_idx = train_set_source_df_idx.reshape((-1, 1))
+    # test_set_source_df_idx = test_set_source_df_idx.reshape((-1, 1))
 
-    x_train = torch.tensor(x_train).float()
-    x_test = torch.tensor(x_test).float()
-    y_train = torch.tensor(y_train).float()
-    y_test = torch.tensor(y_test).float()
-    train_set_source_df_idx = torch.tensor(train_set_source_df_idx)
-    test_set_source_df_idx = torch.tensor(test_set_source_df_idx)
+    # x_train = torch.tensor(x_train).float()
+    # x_test = torch.tensor(x_test).float()
+    # y_train = torch.tensor(y_train).float()
+    # y_test = torch.tensor(y_test).float()
+    # train_set_source_df_idx = torch.tensor(train_set_source_df_idx)
+    # test_set_source_df_idx = torch.tensor(test_set_source_df_idx)
 
-    return (train_dataset, test_dataset,
-            x_train, y_train, x_test, y_test, train_set_source_df_idx, test_set_source_df_idx, scaling_attributes)
+    # return (train_dataset, test_dataset,
+    #         x_train, y_train, x_test, y_test, train_set_source_df_idx, test_set_source_df_idx, scaling_attributes)
+
+    return data_set
--- src/main.py
+++ src/main.py
@@ -1,9 +1,16 @@
+import matplotlib.pyplot as plt
+import darts
+from darts.models import TCNModel
+from darts.utils.likelihood_models import GaussianLikelihood
+from darts.metrics import mae, mape, mse, rmse
+
 import src.framework__data_set as framework__data_set
 import src.utils as utils
 import src.config as config
-import src.models.utils as models_utils
-import src.models.train_and_validate as train_and_validate
 import src.dataset.utils as dataset_utils
+
+import warnings
+warnings.filterwarnings("ignore", category=UserWarning, message="MPS available but not used.")
 
 if __name__ == '__main__':
     utils.seed_everything()
@@ -14,18 +21,95 @@ if __name__ == '__main__':
         path_to_data='../data/'
     )
 
-    model = models_utils.get_model(config.model_name, config.hidden_size, config.num_stacked_layers, config.horizon)
+    data_set = dataset_utils.prepare_data_for_run(data)
 
-    (train_dataset, test_dataset, x_train, y_train, x_test, y_test, train_set_source_df_idx, test_set_source_df_idx,
-     scaling_attributes) = (dataset_utils.prepare_data_for_run(data))
+    # TODO: shuffle between data frames (not within)
 
-    train_and_validate.train_and_validate(model, config.device,
-                                          x_train, y_train, x_test, y_test, train_set_source_df_idx,
-                                          test_set_source_df_idx,
-                                          config.batch_size, config.num_epochs, config.learning_rate,
-                                          config.loss_function, scaling_attributes)
+    # train_ratio = 0.8
+    # num_dfs = len(data_set['source_df_idx'].unique())
+    # num_dfs_train = int(num_dfs * train_ratio)
+    # train_data_set = data_set[data_set['source_df_idx'] <= num_dfs_train]
+    # test_data_set = data_set[data_set['source_df_idx'] > num_dfs_train]
 
-    train_dataset.recover_dfs_from_concatenated()
-    test_dataset.recover_dfs_from_concatenated()
+    # TODO: currently train and val for one df; then extend to all
+    train_df_dataset = data_set[data_set['source_df_idx'] == 0]
 
-    print("Done!")
+    look_back = 12
+    horizon = 2
+
+    model = (
+        TCNModel(
+            dropout=0.2,
+            batch_size=16,
+            n_epochs=500,
+            num_filters=5,
+            num_layers=3,
+            optimizer_kwargs={"lr": 0.0003},
+            random_state=0,
+            input_chunk_length=look_back,
+            output_chunk_length=horizon,
+            kernel_size=5,
+            weight_norm=True,
+            dilation_base=4,
+            likelihood=GaussianLikelihood(),
+            pl_trainer_kwargs={"accelerator": "cpu"},
+        ))
+
+    # TODO: change model num of epochs to 1 for each df and then add num_epochs to the loop
+    # num_epochs = 2
+    # for epoch in range(num_epochs):
+
+    # TODO: currently for one df; then edit to all df in a loop
+    # for df_idx in train_data_set['source_df_idx'].unique():
+    #     subset_source_df_index = train_data_set['source_df_idx'].index[train_data_set['source_df_idx'] == df_idx].tolist()
+    #     subset_data_set = train_data_set.loc[subset_source_df_index]
+
+    darts_train_dataset = darts.TimeSeries.from_dataframe(train_df_dataset,
+                                                          time_col='time',
+                                                          value_cols='sample')
+
+    model.fit(series=darts_train_dataset, past_covariates=None, verbose=True)
+    train_forecast = model.predict(n=len(darts_train_dataset) - look_back, series=darts_train_dataset[:look_back])
+
+    assert len(darts_train_dataset[look_back:]) == len(train_forecast)
+    train_mae = mae(darts_train_dataset[look_back:], train_forecast)
+    train_mape = mape(darts_train_dataset[look_back:], train_forecast)
+    train_mse = mse(darts_train_dataset[look_back:], train_forecast)
+    train_rmse = rmse(darts_train_dataset[look_back:], train_forecast)
+
+    train_forecast.plot(label='Forecast', lw=2)
+    darts_train_dataset.plot(label='Actual')
+    plt.legend()
+    # TODO: add to the plot the df idx
+    plt.title(f'Train Forecasting: '
+              f'MAE = {train_mae:.4f}'
+              f'MAPE = {train_mape:.4f}'
+              f'MSE = {train_mse:.4f}'
+              f'RMSE = {train_rmse:.4f}'
+              )
+    plt.show()
+
+    val_df_dataset = data_set[data_set['source_df_idx'] == 1]
+
+    darts_val_dataset = darts.TimeSeries.from_dataframe(val_df_dataset,
+                                                        time_col='time',
+                                                        value_cols='sample')
+
+    val_forecast = model.predict(n=len(darts_val_dataset) - look_back, series=darts_val_dataset[:look_back])
+    assert len(darts_val_dataset[look_back:]) == len(val_forecast)
+    val_mae = mae(darts_val_dataset[look_back:], val_forecast)
+    val_mape = mape(darts_val_dataset[look_back:], val_forecast)
+    val_mse = mse(darts_val_dataset[look_back:], val_forecast)
+    val_rmse = rmse(darts_val_dataset[look_back:], val_forecast)
+
+    val_forecast.plot(label='Forecast', lw=2)
+    darts_val_dataset.plot(label='Actual')
+    plt.legend()
+    # TODO: add to the plot the df idx
+    plt.title(f'Val Forecasting: '
+              f'MAE = {val_mae:.4f}'
+              f'MAPE = {val_mape:.4f}'
+              f'MSE = {val_mse:.4f}'
+              f'RMSE = {val_rmse:.4f}'
+              )
+    plt.show()
